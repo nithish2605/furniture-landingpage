e2bfb50bfc2954f63c3187c7d4016b3846c3f103
nithishkumar <nithishvj2605@.com>
2026-01-05 16:58:49 +0530

    mobile nav completed and started hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 let menu_bar = document.querySelector('.menu-bar');
 let mobile_navbar = document.querySelector('.mobile-navbar');
+let navbackdrop = document.querySelector('.nav-backdrop');
 
-menu_bar.addEventListener('click',()=>{
+//opening mob navbar
+menu_bar.addEventListener('click',openMenu);
+//closing the mob navbar
+document.querySelector('.close-btn').addEventListener('click',closeMenu);
+navbackdrop.addEventListener('click',closeMenu);
+
+function openMenu(){
   mobile_navbar.classList.add('active');
-})
+  navbackdrop.classList.add('active');
+}
+
+function closeMenu(){
+  mobile_navbar.classList.remove('active');
+  navbackdrop.classList.remove('active');
+}
