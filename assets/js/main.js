09a9027e2cafb56ee9a01b53298c1baa5d3e6875
NithishKumar <nithishvj2605@gmail.com>
2026-01-11 20:17:33 +0530

    completed functionality for cart system

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,9 @@ let menu_bar = document.querySelector('.menu-bar');
 let mobile_navbar = document.querySelector('.mobile-navbar');
 let navbackdrop = document.querySelector('.nav-backdrop');
 let product_box = document.querySelector('.product-container');
-let cart_btn = document.querySelector('.cart-btn');
+let view_Cart = document.querySelector('.view-cart');
+let cart_box = document.querySelector('.cart-box-wrapper');
+let total_price = document.querySelector('.total-price');
 
 //products array
 let products = [
@@ -58,14 +60,121 @@ products.forEach(p => {
                         <p class="m-0 ">Price : ₹${p.price}</p>
                     </div>
                     <div class="action-btns d-flex justify-content-between flex-column flex-xl-row gap-3">
-                        <button class="default-btn cart-btn text-center w-100">Add to cart</button>
+                        <button class="default-btn add-cart-btn text-center w-100" data-id="${p.id}">Add to cart</button>
                         <button class="buy-now-btn  w-100">Buy Now</button>
                     </div>
                 </div>
             </div>`
 });
 
-//add to cart function
-// cart_btn.addEventListener('click',()=>{
+let add_cart_btns = document.querySelectorAll('.add-cart-btn');
+let cart_counter = document.querySelector('.cart-counter');
+let increase_counter = 0;
 
-// });
+//add to cart function
+add_cart_btns.forEach(c => {
+  addToCart(c);
+});
+
+//view cart
+view_Cart.addEventListener('click', () => {
+  cart_box.classList.add('active');
+});
+
+//close cart box
+document.querySelector('.cartbox-close-btn').addEventListener('click', () => {
+  cart_box.classList.remove('active');
+});
+
+//display the cart items
+let cart_items_wrapper = document.querySelector('.cart-items-wrapper');
+
+// function to add items to cart
+function addToCart(c) {
+  c.addEventListener('click', () => {
+    increase_counter++;
+    cart_counter.innerText = increase_counter;
+    let item_id = c.dataset.id;
+    const existing_item = cart_items.find(ci => ci.id === item_id);
+
+    if (existing_item) {
+      existing_item.qty += 1;
+    }
+    else {
+      let getproduct = products.find(i => i.id === item_id);
+      cart_items.push({
+        ...getproduct,
+        qty: 1
+      });
+    }
+    renderCartItems();
+  });
+}
+
+// function to render cart items
+function renderCartItems() {
+  cart_items_wrapper.innerHTML = "";
+  cart_counter.innerText = increase_counter;
+  cart_items.forEach(c => {
+    cart_items_wrapper.innerHTML += `
+  <div class="cart-items mt-3">
+                        <div class="row align-items-center">
+                            <div class="col-4">
+                                <img class="img-fluid" src='${c.path}' alt="">
+                            </div>
+                            <div class="col-8">
+                                <h6>${c.proname}</h6>
+                                <p>₹${c.price}</p>
+                                <div class="quantity-btns bg-grey text-center fw-bold">
+                                    <div class="row justify-content-between align-items-center">
+                                        <div class="col-4">
+                                            <button class="btn btn-dec-q text-white bg-grey" data-id=${c.id}>-</button>
+                                        </div>
+                                        <div class="col-4">
+                                            <small>${c.qty}</small>
+                                        </div>
+                                        <div class="col-4">
+                                            <button class="btn btn-inc-q text-white bg-grey" data-id=${c.id}>+</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`
+  });
+  //calculating total price
+  total_price.innerText = cart_items.reduce((sum, item) => {
+    return sum + (item.price * item.qty);
+  }, 0);
+
+}
+
+cart_items_wrapper.addEventListener('click', (e) => {
+
+  let inc_quantity_btn = e.target.closest('.btn-inc-q');
+  let dec_quantity_btn = e.target.closest('.btn-dec-q');
+
+  //increase quantity
+  if (inc_quantity_btn) {
+    let itemId = inc_quantity_btn.dataset.id;
+    let inc_curr_Item = cart_items.find(i => i.id === itemId);
+    if (inc_curr_Item) {
+      inc_curr_Item.qty++;
+      cart_counter.innerText = increase_counter++;
+    }
+  }
+  //decrease quantity
+  if (dec_quantity_btn) {
+    let itemId = dec_quantity_btn.dataset.id;
+    let dec_curr_Item = cart_items.find(i => i.id === itemId);
+    if (dec_curr_Item) {
+      dec_curr_Item.qty--;
+      cart_counter.innerText = increase_counter--;
+      if (dec_curr_Item.qty <= 0) {
+        cart_items = cart_items.filter(i => i.id != itemId);
+      }
+    }
+  }
+
+  renderCartItems();
+})
